2ccca6662bfe21a215181fe5b22635e19476adda
liuwx199209@163.com <liuwx199209@163.com>
2019-04-08 09:20:22 +0800

    类型强转

--- helloidea/src/com/practice/collection/ArrayListDemo.java
+++ helloidea/src/com/practice/collection/ArrayListDemo.java
@@ -1,5 +1,9 @@
 package com.practice.collection;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @Description:
  * @Author: liuweixin
@@ -7,4 +11,28 @@ package com.practice.collection;
  * @Time: 16:53
  */
 public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(17);
+        list.add(15);
+        list.add(13);
+     /*   Collections.sort(list);
+        System.out.println(list);
+        Integer max = Collections.max(list);
+        Integer min = Collections.min(list);
+        System.out.println(max);
+        System.out.println(min);*/
+
+        castType(list);
+    }
+
+
+
+    public static void castType(List list){
+        List<Integer> list1 = (List<Integer>)list;
+        for (Integer integer : list1){
+            System.out.println(integer);
+        }
+    }
 }
--- helloidea/src/com/practice/demo/Double2IntDemo.java
+++ helloidea/src/com/practice/demo/Double2IntDemo.java
@@ -11,5 +11,12 @@ public class Double2IntDemo {
         double d = 1.234;
         int i = (int)d;
         System.out.println(i);
+
+        Integer i1 = (int)Double.parseDouble("1.23");//转型报错
+        System.out.println(i1);
+
+        String s = "11.2";
+        String[] split = s.split("\\.");//正则要转义
+        System.out.println(split[0]);
     }
 }
